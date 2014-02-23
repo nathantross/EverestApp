176d7b57abd892fd15e4f20b7d2213097c05bb54
Ryan Brandt <Ryabrandt@gmail.com>
2014-02-22 16:35:23 -0800

    basic oauth working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,32 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
 
-def create
-  auth_hash = request.env['omniauth.auth']
+  def create
+    auth_hash = request.env['omniauth.auth']
  
-  if session[:user_id]
-    # Means our user is signed in. Add the authorization to the user
-    User.find(session[:user_id]).add_provider(auth_hash)
- 
-    render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
-  else
-    # Log him in or sign him up
-    auth = Authorization.find_or_create(auth_hash)
- 
-    # Create the session
-    session[:user_id] = auth.user.id
- 
-    render :text => "Welcome #{auth.user.name}!"
-  end
-end
+    @user = User.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||
+    User.create_with_omniauth(auth_hash)
 
-def failure
-  render :text => "Sorry, but you didn't allow access to our app!"
-end
+    session[:user_id] = @user.id 
+
+    redirect_to root_url, :notice => "Signed in!"
+  end
 
   def destroy
-  session[:user_id] = nil
-  render :text => "You've logged out!"
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
 end
 
 end
