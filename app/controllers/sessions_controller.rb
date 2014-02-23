5348ed23fe066ef8ff21459c82d8cfded8e611a6
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-23 15:28:18 -0800

    updates files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,20 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
+
+    render :text => auth_hash.inspect
  
-    @user = User.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||
-    User.create_with_omniauth(auth_hash)
+    #@user = User.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||
+    #User.create_with_omniauth(auth_hash)
 
-    session[:user_id] = @user.id 
+    #session[:user_id] = @user.id 
 
-    redirect_to root_url, :notice => "Signed in!"
+    #redirect_to root_url, :notice => "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
-end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ EverestApp::Application.routes.draw do
   resources :goals
 
   root :to => "sessions#new"
-
-  get '/login', :to => 'sessions#new', :as => :login
-  get '/auth/:provider/callback', :to => 'sessions#create'
-  # get '/auth/failure', :to => 'sessions#failure'
   get '/logout', :to => 'sessions#destroy'
-
+  get   '/login', :to => 'sessions#new', :as => :login
+  get '/auth/:provider/callback', :to => 'sessions#create'
+  get '/auth/failure', :to => 'sessions#failure'
 end
