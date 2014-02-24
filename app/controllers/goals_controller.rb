095bd794d0a3d4ca9b092de65088109c78bbd27c
amandragon <aee8@duke.edu>
2014-02-24 14:14:01 -0800

    show page is correct

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -10,20 +10,21 @@ class GoalsController < ApplicationController
   end
 
   def create
-    query = params[:query]
+    query = params[:goal]
 
     options = { "format" => "plaintext"} # see the reference appendix in the documentation.[1]
     client = WolframAlpha::Client.new "WAH272-2G2QR5X7L6", options
 
-    @response = client.query "distance from earth to moon"
-    # @response = client.query query
+    @response = client.query query["name"]
 
+    # @response = client.query query
+    # binding.pry
     result = @response.find { |pod| pod.id == "Result" }
-    answer = result.subpods[0].plaintext
+    answer = result.subpods.first.plaintext
 
     @goal = Goal.new
-    @goal.name = "input from form here"
-    @goal.distance =1.1
+    @goal.name = query["name"]
+    # @goal.input_interpretation = @goal.input_interpretation
     @goal.input_interpretation = answer
     @goal.save
 
@@ -32,7 +33,6 @@ class GoalsController < ApplicationController
   end
 
   def show
-    # @goal = Goal.find(params[:input_interpretation, :distance])
     @goal = Goal.find(params[:id])
   end
 
@@ -40,14 +40,14 @@ class GoalsController < ApplicationController
     @goal = Goal.find(params[:id])
   end
 
-  def update
-    goal = Goal.find(params[:id])
-    updated_goal = params.require(:goal).permit(:name, :distance)
-    goal.update_attributes(updated_goal)
-    if goal.save
-      redirect_to goal
-    end
-  end
+  # def update
+  #   goal = Goal.find(params[:id])
+  #   updated_goal = params.require(:goal).permit(:name, :distance)
+  #   goal.update_attributes(updated_goal)
+  #   if goal.save
+  #     redirect_to goal
+  #   end
+  # end
 
   def delete
     goal = Goal.find(params[:id])
