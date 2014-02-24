2f7bf5bbd307b909e3108e310e95bde4b27b0fff
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-24 12:59:57 -0800

    fixes confclits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-    auth_hash = request.env['omniauth.auth']
-
-    render :text => auth_hash.inspect
- 
-    #@user = User.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||
-    #User.create_with_omniauth(auth_hash)
-
-    #session[:user_id] = @user.id 
-
-    #redirect_to root_url, :notice => "Signed in!"
-=======
     auth = request.env["omniauth.auth"]
-    binding.pry
+ 
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
->>>>>>> 527298e70cfb9800eacf814d7e4c4ad572561f95
   end
 
   def destroy
