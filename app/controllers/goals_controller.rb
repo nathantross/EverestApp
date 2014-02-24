cd1eee2450d428e4aa4c9c301752c42d084e4878
amandragon <aee8@duke.edu>
2014-02-24 12:32:36 -0800

    api is working

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,6 @@
 class GoalsController < ApplicationController
+  require 'wolfram-alpha'
+
   def index
     @goals = Goal.all
   end
@@ -8,14 +10,29 @@ class GoalsController < ApplicationController
   end
 
   def create
-    new_goal = params.require(:goal).permit(:name, :distance)
-    @goal = Goal.create(new_goal)
-    if @goal.save
-      redirect_to @goal
-    end
+    query = params[:query]
+
+    options = { "format" => "plaintext"} # see the reference appendix in the documentation.[1]
+    client = WolframAlpha::Client.new "WAH272-2G2QR5X7L6", options
+
+    @response = client.query "distance from earth to moon"
+    # @response = client.query query
+
+    result = @response.find { |pod| pod.id == "Result" }
+    answer = result.subpods[0].plaintext
+
+    @goal = Goal.new
+    @goal.name = "input from form here"
+    @goal.distance =1.1
+    @goal.input_interpretation = answer
+    @goal.save
+
+    render :show
+
   end
 
   def show
+    # @goal = Goal.find(params[:input_interpretation, :distance])
     @goal = Goal.find(params[:id])
   end
 
