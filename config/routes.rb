5f288470d050235178c437b368320ac035ddbd3e
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-24 13:02:14 -0800

    fixes conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,7 @@ EverestApp::Application.routes.draw do
   get "/about" => "users#about"
 
   get '/auth/:provider/callback', :to => 'sessions#create'
-<<<<<<< HEAD
-  # get '/auth/failure', :to => 'sessions#failure'
-=======
+
   get '/logout', :to => 'sessions#destroy'
 
->>>>>>> 527298e70cfb9800eacf814d7e4c4ad572561f95
 end
