8f063851598ee742d0db334ab6787703a95710ad
nathantross <nathantross@gmail.com>
2014-02-25 10:45:50 -0800

    redirected user away from Session New page if logged in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    if current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to user_path(current_user.id), :notice => "Signed in!"
   end
 
   def destroy
