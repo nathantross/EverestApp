8f0f79be5ddad4105bbdcba79816278d9ff16331
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-25 15:37:15 -0800

    updates files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
   # Include Factory Girl syntax to simplify calls to factories 
   config.include FactoryGirl::Syntax::Methods
+
+  config.include LoginMacros
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
