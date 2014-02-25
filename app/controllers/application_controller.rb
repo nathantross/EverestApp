25acbc51de265b19ef35e0cd6325225f5c348d95
Ryan Brandt <Ryabrandt@gmail.com>
2014-02-24 23:54:10 -0800

    added functionality to access fitbit info in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   helper_method :current_user
+  helper_method :fitgem_info
+  helper_method :distance
+  helper_method :avatar
+  helper_method :climb
+
+  def fitgem_info
+    @fitgem_info = current_user.fitbit_data
+  end
+
+  def distance
+    @distance = fitgem_info.activity_statistics['lifetime']['tracker']['distance']
+  end
+
+  def avatar
+    @avatar = fitgem_info.user_info['user']['avatar']
+  end
+
+  def climb
+    @climb = fitgem_info.activity_statistics['lifetime']['tracker']['floors'] * 10
+  end
+
   private
 
   def current_user
