02100d24f30fc9aa8f783bb7cdcd3cc93c5b3b70
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-25 14:30:58 -0800

    updates spec files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-   def index
-     @users = User.all
-   end
- 
-   def show
-     @user = User.find(params[:id])
-     @distance = fitgem_info.activity_statistics['lifetime']['tracker']['distance']
-     @avatar = fitgem_info.user_info['user']['avatar']
-     @climb = fitgem_info.activity_statistics['lifetime']['tracker']['floors'] * 10
-    end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @distance = fitgem_info.activity_statistics['lifetime']['tracker']['distance']
+    @avatar = fitgem_info.user_info['user']['avatar']
+    @climb = fitgem_info.activity_statistics['lifetime']['tracker']['floors'] * 10
+  end
 end
