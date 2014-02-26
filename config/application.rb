c08ead46fc7fb2d98c7b31bbfe03aa30bc4d4b03
Ryan Brandt <Ryabrandt@gmail.com>
2014-02-26 13:59:54 -0800

    serving static assets true again

--- config/application.rb
+++ config/application.rb
@@ -10,20 +10,20 @@ Bundler.require(:default, Rails.env)
 module EverestApp
   class Application < Rails::Application
     config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(css|js)\z/
-    full_path = Rails.application.assets.resolve(path).to_path
-    app_assets_path = Rails.root.join('app', 'assets').to_path
-    if full_path.starts_with? app_assets_path
-      puts "including asset: " + full_path
-      true
-    else
-      puts "excluding asset: " + full_path
-      false
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
     end
-  else
-    false
-  end
-end
 
     config.generators do |g|
       g.test_framework :rspec,
