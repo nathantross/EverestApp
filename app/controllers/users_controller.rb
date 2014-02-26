8f07b7c2587a604404f6a77b1dece91ae70920ab
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-25 20:15:17 -0800

    cleans up user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,5 @@ class UsersController < ApplicationController
  
    def show
      @user = User.find(params[:id])
-     # @distance = fitgem_info.activity_statistics['lifetime']['tracker']['distance']
-     # @avatar = fitgem_info.user_info['user']['avatar']
-     # @climb = fitgem_info.activity_statistics['lifetime']['tracker']['floors'] * 10
    end
 end
