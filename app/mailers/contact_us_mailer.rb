dee05fbafc90b668762a6c2459ac70e4b67da0b5
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-27 13:41:06 -0800

    adds contact team link to footer and add reply to funcitonality

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,11 +1,10 @@
 class ContactUsMailer < ActionMailer::Base
-  default from: "http://localhost:3000/contact"
+  
+  default from: "http://salty-brook-3000.herokuapp.com/contact"
   default to: "Ryabrandt@gmail.com, eganae@gmail.com, nathantross@gmail.com, arweiner@alumni.stanford.edu"
-  default reply_to: "test@example.com"
-    
+
   def contact_us_email(contact)
     @contact = contact
-    @url  = 'http://localhost:3000/contact'
-    mail(subject: "Everest App User Query")
+    mail(subject: "Everest App User Query", reply_to: @contact.email)
   end
 end
