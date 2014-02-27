042d7806617fc443aa4a9804b037c6379794bf45
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-26 16:13:42 -0800

    updates styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 EverestApp::Application.routes.draw do
-  get '/users/run', to: 'users#run'
-  resources :users
-
+  # Home page
   root :to => "sessions#new"
 
-  get "/about" => "users#about"
-
+  # Sessions
   get '/auth/:provider/callback', :to => 'sessions#create'
-
   get '/logout', :to => 'sessions#destroy'
 
+  # Users
+  get "/about" => "users#about"
+  get '/users/run', to: 'users#run'
+
+  # Goals
   get '/goals', to: 'goals#index', as: 'goals'
   get '/goals/new', to: 'goals#new', as: 'new_goal'
   post '/goals', to: 'goals#create'
