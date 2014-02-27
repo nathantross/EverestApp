2f7d60acf34d8ec0a64ebea68cc73b11802d315e
amandragon <aee8@duke.edu>
2014-02-27 11:32:06 -0800

    commit

--- db/migrate/20140225230032_add_distance_to_goal.rb
+++
@@ -1,5 +0,0 @@
-class AddDistanceToGoal < ActiveRecord::Migration
-  def change
-    add_column :goals, :distance, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140225230032) do
-=======
 ActiveRecord::Schema.define(version: 20140226014053) do
->>>>>>> 041caaf9370bdfa2e99fde4431f316ef85950f20
-=======
-ActiveRecord::Schema.define(version: 20140226014053) do
->>>>>>> 041caaf9370bdfa2e99fde4431f316ef85950f20
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +22,6 @@ ActiveRecord::Schema.define(version: 20140226014053) do
     t.float    "distance"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "distance"
   end
 
   create_table "users", force: true do |t|
