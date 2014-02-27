71ea79a4146bd70fe744838db58cfc3cce19eb1e
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-27 12:47:24 -0800

    updates files

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,7 +1,8 @@
 class ContactUsMailer < ActionMailer::Base
-  default to: "arweiner@alumni.stanford.edu"
-  default reply_to: @contact.email
-
+  default from: "http://localhost:3000/contact"
+  default to: "Ryabrandt@gmail.com, eganae@gmail.com, nathantross@gmail.com, arweiner@alumni.stanford.edu"
+  default reply_to: "test@example.com"
+    
   def contact_us_email(contact)
     @contact = contact
     @url  = 'http://localhost:3000/contact'
